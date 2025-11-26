088c3e7caa915ee0fc984659d7f6ce8a6726411c
gianhu1991 <gianhu1991@gmail.com>
2025-11-26 16:00:15 +0700

    Fix navigation title

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -64,7 +64,7 @@ export default function Navigation() {
             <svg className="w-8 h-8" fill="none" stroke="currentColor" viewBox="0 0 24 24">
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" />
             </svg>
-            Thi Trắc Nghiệm Online
+            TTVT Nho Quan- Phần mềm đào tạo kỹ thuật
           </Link>
           {isAuthenticated && (
             <div className="hidden md:flex gap-6 items-center">
