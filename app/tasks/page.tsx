9346078d4ac44ae7412644be2d872fcb57aa420f
gianhu1991 <gianhu1991@gmail.com>
2025-12-05 16:49:27 +0700

    Fix JSX syntax error in tasks page modal - Close div before closing ternary operator - Fix structure: div -> fragment -> close fragment -> close div -> close ternary

--- app/tasks/page.tsx
+++ app/tasks/page.tsx
@@ -1339,7 +1339,6 @@ export default function TasksPage() {
                   )}
                 </div>
               </>
-            ) : null}
             </div>
           ) : null}
         </div>
